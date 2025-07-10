bb1153615c42756d24a2d37ddfde8c42a08266db
rinaresa <rahinamoro58@gmail.com>
2025-07-10 19:20:36 +0000

    Add bookshelf app and CRUD operations for Book model

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',  # 👈 Add your app here
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
