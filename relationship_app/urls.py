34a24147d110cd43da7ead1883c5463490fe4b5e
rinaresa <rahinamoro58@gmail.com>
2025-07-20 11:49:41 +0000

    Fixed template structure and implemented library detail view

--- relationship_app/urls.py
+++ relationship_app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import home, book_list, library_detail, LibraryDetailView
+from .views import book_list, LibraryDetailView
 
 urlpatterns = [
-    path('', home, name='home'),
     path('books/', book_list, name='book-list'),
-    path('library/<int:pk>/fbv', library_detail, name='library-detail-fbv'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
 ]
--- relationship_app/views.py
+++ relationship_app/views.py
@@ -1,33 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library, Author
+from .models import Book, Library
 
-def home(request):
-    """Home page view"""
-    libraries = Library.objects.all()
-    return render(request, 'relationship_app/home.html', {'libraries': libraries})
-
+# Function-based view for listing all books
 def book_list(request):
-    """Book list view"""
-    books = Book.objects.all()
+    books = Book.objects.all().select_related('author')  # Optimized query
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-def library_detail(request, pk):
-    """Function-based view for library details"""
-    library = Library.objects.get(pk=pk)
-    books = library.books.all()  # Using the related_name 'books'
-    return render(request, 'relationship_app/library_detail.html', {
-        'library': library,
-        'books': books
-    })
-
+# Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Get all books in this library using the many-to-many relationship
-        context['books'] = self.object.books.all()
+        # No need to add books - they're accessible via library.books.all in template
         return context
