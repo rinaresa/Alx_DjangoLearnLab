a223dd3fd30d2614f1f33cc7440c3ac0475d725c
rinaresa <rahinamoro58@gmail.com>
2025-07-26 14:39:57 +0000

    Restore django-models directory with Introduction_to_Django content

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-   ## list_display = ('title', 'author')
+    list_display =('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
-    list_filter = ('author',)
-
-
+    list_filter = ('publication_year',)
--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import book_list, home, LibraryDetailView  # Make sure all views are imported
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('books/', book_list, name='book-list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-]
