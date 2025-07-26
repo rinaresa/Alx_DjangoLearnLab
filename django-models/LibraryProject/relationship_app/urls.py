5b7792c544a1f5633508d27cc9e1c1c729d50e76
rinaresa <rahinamoro58@gmail.com>
2025-07-26 20:25:47 +0000

    Add Admin-only dashboard view with role-based access control

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
 ]
