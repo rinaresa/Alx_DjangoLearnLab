20acec6040310b87d3c1c7b818ae5349b7e13c7d
rinaresa <rahinamoro58@gmail.com>
2025-07-28 20:31:01 +0000

    Add custom user model in bookshelf with settings and admin integration

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# ✅ Custom user model
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
