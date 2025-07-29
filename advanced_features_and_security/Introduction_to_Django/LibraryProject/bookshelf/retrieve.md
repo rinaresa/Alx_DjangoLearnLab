b6b337a6e3b574f649b0fb036f8524b2e67c8f64
rinaresa <rahinamoro58@gmail.com>
2025-07-29 14:27:02 +0000

    Added book_list to views.py in bookshelf

---
+++ advanced_features_and_security/Introduction_to_Django/LibraryProject/bookshelf/retrieve.md
@@ -0,0 +1,8 @@
+
+### RETRIEVE OPERATION
+
+```python
+from bookshelf.models import Book
+book = Book.objects.get(title="1984")
+print(book.title, book.author, book.publication_year)
+# 1984 George Orwell 1949
---
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Additional Info'), {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_('Additional Info'), {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth']
+    search_fields = ['username', 'email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
