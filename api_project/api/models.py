a5ecb548834e61a4a954d8f2d7d8084bfb3f82c7
rinaresa <rahinamoro58@gmail.com>
2025-08-05 13:38:45 +0000

    Add Author and Book models

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
 
     def __str__(self):
         return self.title
