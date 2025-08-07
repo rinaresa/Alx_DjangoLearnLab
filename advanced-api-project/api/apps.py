de25d42725769c42f45c11e559bb77054a0faf0a
rinaresa <rahinamoro58@gmail.com>
2025-08-07 18:22:02 +0000

    Initial setup: add Author and Book models, serializers, and migrations

--- advanced-api-project/api/apps.py
+++ advanced-api-project/api/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
 
+
 class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-    verbose_name = 'API Application'
-    
-    def ready(self):
-        # This forces model registration
-        from . import models
-        models.Author
-        models.Book
