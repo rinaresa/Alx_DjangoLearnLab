440439bb1e19afcbefe78166b0247f820afc8bb2
rinaresa <rahinamoro58@gmail.com>
2025-08-08 06:54:07 +0000

    Fix: Add required permission imports to views\

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,5 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
