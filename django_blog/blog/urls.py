3f0e628333252ddaa3b9da65600437e292fd6987
rinaresa <rahinamoro58@gmail.com>
2025-08-17 01:03:54 +0000

    Added user authentication system with registration, login, logout, and profile management

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .views import PostListView, PostDetailView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+]
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 class PostListView(ListView):
     model = Post
@@ -10,3 +14,19 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
